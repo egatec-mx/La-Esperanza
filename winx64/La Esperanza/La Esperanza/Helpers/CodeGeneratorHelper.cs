1b053078e424a742415b7bf818dc3c0bbff62687
Efrain Garcia <egatec@hotmail.com>
2025-09-10 10:18:33 -0600

    Actualización de Solución
    Se quitó la referencia a Barcode Express, código propietario, y se actualizó la referencia de Destino a .NET 8

--- winx64/La Esperanza/La Esperanza/Helpers/CodeGeneratorHelper.cs
+++ winx64/La Esperanza/La Esperanza/Helpers/CodeGeneratorHelper.cs
@@ -1,7 +1,4 @@
-﻿using Barcode.Express.Core;
-using System;
-using System.Drawing;
-using System.Drawing.Imaging;
+﻿using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -15,16 +12,9 @@ namespace La_Esperanza.Helpers
             {
                 using (MemoryStream memoryStream = new MemoryStream())
                 {
-                    using (QRCode qrCode = new QRCode
-                    {
-                        BackgroundColor = Color.White,
-                        ForeColor = Color.Black,
-                        Size = 5,
-                        Value = OrderId
-                    })
-                    {
-                        qrCode.Draw().Save(memoryStream, ImageFormat.Png);
-                    }
+                    // Removed EGATEC™ Barcode Express Propietary Code
+                    // Because it is not free.
+                    // You can use any other library to generate the barcode image.
                     string base64Img = Convert.ToBase64String(memoryStream.GetBuffer());
                     return base64Img;
                 }
